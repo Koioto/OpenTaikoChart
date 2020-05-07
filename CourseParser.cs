ce0180a64ef948933f0e4b2d3808bebaa3cc330c
AioiLight <info@aioilight.space>
2020-05-07 20:26:07 +0900

    サンプル用

---
+++ CourseParser.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Koioto.Support;
+using Koioto.Support.FileReader;
+
+namespace Koioto.SamplePlugin.OpenTaikoChart
+{
+    public static class CourseParser
+    {
+        public static Playable Parse(OpenTaikoChartInfomation otci, OpenTaikoChartCourse otcc)
+        {
+            // メドレーではないので大きさは1。
+            var playable = new Playable();
+            playable.Sections = new List<Chip>[1];
+            var sections = playable.Sections;
+            sections[0] = new List<Chip>();
+
+            var courseJson = otcc;
+
+            // パースする。
+            var list = sections[0];
+
+            // 初めから1秒開けておく。
+
+            var nowTime = 1000000L;
+            var nowBPM = otci.BPM ?? 120.0;
+            var nowMeasure = new Measure(4, 4);
+            var nowScroll = 1.0;
+            var isGoGoTime = false;
+            var measureCount = 0;
+            var isFirstNoteInMeasure = true;
+            var movieOffset = otci.Movieoffset;
+            Chip rollstartChip = null;
+
+            // オフセットする。
+            var offset = otci.Offset ?? 0;
+            // オフセットが0未満だったらBGMの開始を0にしてそれ以降をずらす。
+            // そうでないならそのままオフセットする。
+            if (offset < 0)
+            {
+                //
+                //var oneMeasure = GetMeasureDuration(nowMeasure, nowBPM);
+                var bgmStartChip = new Chip();
+                bgmStartChip.ChipType = Chips.BGMStart;
+                bgmStartChip.Time = nowTime + (long)(Math.Abs(offset) * 1000.0 * 1000.0);
+                list.Add(bgmStartChip);
+            }
+            else
+            {
+                var bgmStartChip = new Chip();
+                bgmStartChip.ChipType = Chips.BGMStart;
+                bgmStartChip.Time = nowTime;
+                list.Add(bgmStartChip);
+                // その時間分ずらす
+                nowTime += (long)(offset * 1000.0 * 1000.0);
+            }
+
+            var origin = list.Where(c => c.ChipType == Chips.BGMStart).First();
+
+            foreach (var measure in courseJson.Measures)
+            {
+                // その小節にいくつ音符があるかカウントする
+                var notesCount = 0;
+                foreach (var line in measure)
+                {
+                    if (!line.Trim().StartsWith("#"))
+                    {
+                        // 命令行ではない
+                        foreach (var digit in line)
+                        {
+                            if (digit >= '0' && digit <= '9')
+                            {
+                                // 数字だ
+                                notesCount++;
+                            }
+                        }
+                    }
+                }
+
+                foreach (var line in measure)
+                {
+                    // 行
+                    // ひとつの音符あたりの数
+                    var timePerNotes = (long)(GetMeasureDuration(nowMeasure, nowBPM) / notesCount);
+
+                    if (!line.Trim().StartsWith("#"))
+                    {
+                        // 命令行ではない
+                        if (isFirstNoteInMeasure)
+                        {
+                            // 小節
+                            var measureChip = new Chip();
+                            measureChip.ChipType = Chips.Measure;
+                            measureChip.CanShow = true;
+                            measureChip.Scroll = nowScroll;
+                            measureChip.BPM = nowBPM;
+                            measureChip.IsGoGoTime = isGoGoTime;
+                            measureChip.Measure = nowMeasure;
+                            measureChip.MeasureCount = measureCount;
+                            measureChip.Time = nowTime;
+                            list.Add(measureChip);
+
+                            isFirstNoteInMeasure = false;
+                        }
+                        foreach (var digit in line)
+                        {
+                            if (digit >= '0' && digit <= '9')
+                            {
+                                // 数字だ
+                                var note = NotesConverter.GetNotesFromChar(digit);
+
+                                // 音符の追加
+                                var noteChip = new Chip();
+                                noteChip.ChipType = Chips.Note;
+                                noteChip.NoteType = note;
+                                noteChip.Scroll = nowScroll;
+                                noteChip.BPM = nowBPM;
+                                noteChip.CanShow = true;
+                                noteChip.IsGoGoTime = isGoGoTime;
+                                noteChip.Measure = nowMeasure;
+                                noteChip.MeasureCount = measureCount;
+                                noteChip.Time = nowTime;
+
+                                if (note == Notes.RollStart || note == Notes.ROLLStart || note == Notes.Balloon)
+                                {
+                                    // 連打開始なので記憶しておく。
+                                    rollstartChip = noteChip;
+
+                                    if (note == Notes.Balloon)
+                                    {
+                                        if (courseJson.Balloon.Length > BalloonIndex)
+                                        {
+                                            if (courseJson.Balloon[BalloonIndex].HasValue)
+                                            {
+                                                noteChip.RollCount = courseJson.Balloon[BalloonIndex].Value;
+                                            }
+                                            else
+                                            {
+                                                noteChip.RollCount = 5;
+                                            }
+                                            BalloonIndex++;
+                                        }
+                                        else
+                                        {
+                                            noteChip.RollCount = 5;
+                                        }
+                                    }
+                                }
+                                else if (note == Notes.RollEnd)
+                                {
+                                    // 連打開始なので記憶した連打をrollendにり当てる。
+                                    if (rollstartChip != null)
+                                    {
+                                        rollstartChip.RollEnd = noteChip;
+                                        rollstartChip = null;
+                                    }
+                                }
+
+                                list.Add(noteChip);
+
+                                // 時間をひとつすすめる
+                                nowTime += timePerNotes;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // 命令行
+                        var eventChip = new Chip();
+
+                        var command = line.Trim();
+                        var param = command.IndexOf(' ') >= 0 ? command.Substring(command.IndexOf(' ')) : "";
+
+                        bool commandMatch(string name)
+                        {
+                            return command.StartsWith(name, StringComparison.InvariantCultureIgnoreCase);
+                        }
+
+                        if (commandMatch("#bpm"))
+                        {
+                            // #bpm n1
+                            if (double.TryParse(param, out var bpm))
+                            {
+                                eventChip.ChipType = Chips.BPMChange;
+                                nowBPM = bpm;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (commandMatch("#scroll"))
+                        {
+                            // #scroll n1
+                            if (double.TryParse(param, out var scroll))
+                            {
+                                eventChip.ChipType = Chips.ScrollChange;
+                                nowScroll = scroll;
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (commandMatch("#tsign"))
+                        {
+                            // #tsign n1/n2
+                            var n1 = param.Substring(0, param.IndexOf('/'));
+                            var n2 = param.Substring(param.IndexOf('/') + 1);
+
+                            if (double.TryParse(n1, out var part) && double.TryParse(n2, out var beat))
+                            {
+                                eventChip.ChipType = Chips.MeasureChange;
+                                nowMeasure = new Measure(part, beat);
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else if (commandMatch("#gogobegin"))
+                        {
+                            eventChip.ChipType = Chips.GoGoStart;
+                            isGoGoTime = true;
+                        }
+                        else if (commandMatch("#gogoend"))
+                        {
+                            eventChip.ChipType = Chips.GoGoEnd;
+                            isGoGoTime = false;
+                        }
+                        else if (commandMatch("#delay"))
+                        {
+                            // #delay n1
+                            if (double.TryParse(param, out var delay))
+                            {
+                                nowTime += (long)(delay * 1000.0 * 1000.0);
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                        eventChip.Scroll = nowScroll;
+                        eventChip.BPM = nowBPM;
+                        eventChip.IsGoGoTime = isGoGoTime;
+                        eventChip.Measure = nowMeasure;
+                        eventChip.MeasureCount = measureCount;
+                        eventChip.Time = nowTime;
+                        list.Add(eventChip);
+                    }
+                }
+                measureCount++;
+                isFirstNoteInMeasure = true;
+            }
+
+            // 後処理。
+
+            // 動画の開始時間を表すチップを挿入する。
+            if (movieOffset.HasValue)
+            {
+                var offsetValue = movieOffset.Value;
+                // 0未満だったら、全体をずらす必要がある。
+                if (offsetValue < 0)
+                {
+                    // すべてのチップの時間をずらす
+                    list.ForEach(c => c.Time += (long)(Math.Abs(offsetValue) * 1000.0 * 1000.0));
+
+                    var offsetChip = new Chip();
+                    offsetChip.ChipType = Chips.MovieStart;
+                    offsetChip.Time = origin.Time - (long)(Math.Abs(offsetValue) * 1000.0 * 1000.0);
+                    //list.Add(offsetChip);
+                    var nearestChip = list.Where(c => c.Time <= offsetChip.Time);
+                    list.Insert(nearestChip.Count() > 0 ? list.IndexOf(nearestChip.Last()) : 0, offsetChip);
+                }
+                else
+                {
+                    // そのまま入れることができる。
+                    var offsetChip = new Chip();
+                    offsetChip.ChipType = Chips.MovieStart;
+                    offsetChip.Time = origin.Time + (long)(Math.Abs(offsetValue) * 1000.0 * 1000.0);
+                    var nearestChip = list.Where(c => c.Time <= offsetChip.Time);
+                    list.Insert(nearestChip.Count() > 0 ? list.IndexOf(nearestChip.Last()) : 0, offsetChip);
+                }
+            }
+
+            return playable;
+        }
+
+        /// <summary>
+        /// 1小節の時間を求める。
+        /// </summary>
+        /// <param name="measure">n分のn拍子。</param>
+        /// <param name="bpm">BPM。</param>
+        /// <returns>1小節の時間(秒)</returns>
+        private static double GetMeasureDuration(Measure measure, double bpm)
+        {
+            return measure.GetRate() / bpm * 1000 * 1000.0;
+        }
+
+        private static int BalloonIndex;
+    }
+}
---
+++ FileReader.cs
@@ -0,0 +1,299 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using Koioto.Support;
+using Newtonsoft.Json;
+
+namespace Koioto.SamplePlugin.OpenTaikoChart
+{
+    /// <summary>
+    /// Open Taiko Chart Rev.1 に準拠したクラス。
+    /// </summary>
+    public class FileReader : Koioto.Plugin.IFileReadable
+    {
+        public string Name => "Open Taiko Chart";
+
+        public string[] Creator => new string[] { "AioiLight" };
+
+        public string Description => "Koioto file Reader plugin for Open Taiko Chart.";
+
+        public string Version => "1.0";
+
+        public string[] GetExtensions()
+        {
+            // サポートする拡張子をreturn。
+            return new string[] { ".tci", ".tcm" };
+        }
+
+        public Infomationable GetSelectable(string filePath)
+        {
+            if (Path.GetExtension(filePath) == ".tci")
+            {
+                return TCIParser(filePath);
+            }
+            else if (Path.GetExtension(filePath) == ".tcm")
+            {
+                return TCMParser(filePath);
+            }
+            return null;
+        }
+
+        private Infomationable TCIParser(string filePath)
+        {
+            var infoText = File.ReadAllText(filePath, Encoding.UTF8);
+            var info = JsonConvert.DeserializeObject<OpenTaikoChartInfomation>(infoText);
+
+            var courses = new OpenTaikoChart_Difficluty[info.Courses.Length];
+            for (var courseIndex = 0; courseIndex < courses.Length; courseIndex++)
+            {
+                // SP譜面
+                var path = Path.Combine(Path.GetDirectoryName(filePath), info.Courses[courseIndex].Single);
+                if (!File.Exists(path))
+                {
+                    courses[courseIndex] = null;
+                    continue;
+                }
+                var single = File.ReadAllText(path, Encoding.UTF8);
+                courses[courseIndex] = new OpenTaikoChart_Difficluty();
+                courses[courseIndex].Single = JsonConvert.DeserializeObject<OpenTaikoChartCourse>(single);
+
+                // DP譜面
+                if (info.Courses[courseIndex].Multiple == null)
+                {
+                    continue;
+                }
+
+                var multipleAmount = info.Courses[courseIndex].Multiple.Length;
+                courses[courseIndex].Multiple = new OpenTaikoChartCourse[multipleAmount];
+                for (int multipleIndex = 0; multipleIndex < multipleAmount; multipleIndex++)
+                {
+                    var multiple = File.ReadAllText(Path.Combine(Path.GetDirectoryName(filePath), info.Courses[courseIndex].Multiple[multipleIndex]), Encoding.UTF8);
+                    courses[courseIndex].Multiple[multipleIndex] = JsonConvert.DeserializeObject<OpenTaikoChartCourse>(multiple);
+                }
+            }
+
+            var result = new Infomationable();
+            result.FilePath = filePath;
+            result.Title = info.Title;
+            result.SubTitle = info.Subtitle;
+            result.BPM = info.BPM;
+            result.Artist = info.Artist;
+            result.Creator = info.Creator;
+            if (info.Audio != null)
+            {
+                result.PreviewSong = Path.Combine(Path.GetDirectoryName(filePath), info.Audio);
+            }
+            else
+            {
+                result.PreviewSong = null;
+            }
+            result.SongPreviewTime = info.SongPreview;
+            result.AlbumartPath = info.Albumart;
+
+            foreach (var item in info.Courses)
+            {
+                var diff = new Difficulty();
+                diff.Level = item.Level;
+                result[GetCoursesFromStirng(item.Difficulty)] = diff;
+            }
+
+            return result;
+        }
+
+        private Infomationable TCMParser(string filePath)
+        {
+            // not implemented
+            return null;
+        }
+
+        public (Playable[], ChartInfo) GetPlayable(string filePath, Koioto.Support.FileReader.Courses courses)
+        {
+            if (Path.GetExtension(filePath) == ".tci")
+            {
+                return TCIPlayable(filePath, courses);
+            }
+            else if (Path.GetExtension(filePath) == ".tcm")
+            {
+                return TCMPlayable(filePath, courses);
+            }
+            return (null, null);
+        }
+
+        private (Playable[], ChartInfo) TCIPlayable(string filePath, Koioto.Support.FileReader.Courses course)
+        {
+            var infoText = File.ReadAllText(filePath, Encoding.UTF8);
+            var info = JsonConvert.DeserializeObject<OpenTaikoChartInfomation>(infoText);
+
+            var courses = new OpenTaikoChart_Difficluty[info.Courses.Length];
+
+            // とりあえず、難易度を入れる
+
+            var neededCourse = info.Courses.Where(d => GetCoursesFromStirng(d.Difficulty) == course);
+
+            if (!neededCourse.Any())
+            {
+                // コースが無ければnullを返す。
+                return (null, null);
+            }
+
+            // コースが存在すれば処理を続行。
+            var diff = neededCourse.First();
+
+            var singleFile = File.ReadAllText(Path.Combine(Path.GetDirectoryName(filePath), diff.Single), Encoding.UTF8);
+
+            var multipleFile = new string[0];
+            if (diff.Multiple != null)
+            {
+                multipleFile = new string[diff.Multiple.Length];
+                for (int i = 0; i < multipleFile.Length; i++)
+                {
+                    multipleFile[i] = File.ReadAllText(Path.Combine(Path.GetDirectoryName(filePath), diff.Multiple[i]), Encoding.UTF8);
+                }
+            }
+
+            var single = JsonConvert.DeserializeObject<OpenTaikoChartCourse>(singleFile);
+            var multiple = new OpenTaikoChartCourse[multipleFile.Length];
+            for (int i = 0; i < multiple.Length; i++)
+            {
+                multiple[i] = JsonConvert.DeserializeObject<OpenTaikoChartCourse>(multipleFile[i]);
+            }
+
+            var resultJson = new OpenTaikoChartCourse[] { single }.Concat(multiple);
+            var playable = resultJson.Select(c => CourseParser.Parse(info, c)).ToArray();
+
+            var chartInfo = new ChartInfo();
+
+            chartInfo.Title = new string[1];
+            chartInfo.Title[0] = info.Title;
+
+            chartInfo.Subtitle = new string[1];
+            chartInfo.Subtitle[0] = info.Subtitle;
+
+            chartInfo.Artist = new string[1][];
+            chartInfo.Artist[0] = new string[info.Artist.Length];
+            chartInfo.Artist[0] = info.Artist;
+
+            chartInfo.Creator = new string[1][];
+            chartInfo.Creator[0] = new string[info.Creator.Length];
+            chartInfo.Creator[0] = info.Creator;
+
+            chartInfo.Audio = new string[1];
+            if (info.Audio != null)
+            {
+                chartInfo.Audio[0] = Path.Combine(Path.GetDirectoryName(filePath), info.Audio);
+            }
+            else
+            {
+                chartInfo.Audio[0] = null;
+            }
+
+            chartInfo.Background = new string[1];
+            if (info.Background != null)
+            {
+                chartInfo.Background[0] = Path.Combine(Path.GetDirectoryName(filePath), info.Background);
+            }
+            else
+            {
+                chartInfo.Background[0] = null;
+            }
+
+            chartInfo.Movieoffset = new double?[1];
+            chartInfo.Movieoffset[0] = info.Movieoffset;
+
+            chartInfo.BPM = new double?[1];
+            chartInfo.BPM[0] = info.BPM;
+
+            chartInfo.Offset = new double?[1];
+            chartInfo.Offset[0] = info.Offset;
+
+            return (playable, chartInfo);
+        }
+
+        private (Playable[], ChartInfo) TCMPlayable(string filePath, Koioto.Support.FileReader.Courses courses)
+        {
+            return (null, null);
+        }
+
+        private Koioto.Support.FileReader.Courses GetCoursesFromStirng(string str)
+        {
+            switch (str)
+            {
+                case "easy":
+                    return Koioto.Support.FileReader.Courses.Easy;
+
+                case "normal":
+                    return Koioto.Support.FileReader.Courses.Normal;
+
+                case "hard":
+                    return Koioto.Support.FileReader.Courses.Hard;
+
+                case "edit":
+                    return Koioto.Support.FileReader.Courses.Edit;
+
+                case "oni":
+                default:
+                    return Koioto.Support.FileReader.Courses.Oni;
+            }
+        }
+    }
+
+    public class OpenTaikoChartInfomation
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string[] Artist { get; set; }
+        public string[] Creator { get; set; }
+        public string Audio { get; set; }
+        public string Background { get; set; }
+        public double? Movieoffset { get; set; }
+        public double? BPM { get; set; }
+        public double? Offset { get; set; }
+        public double? SongPreview { get; set; }
+        public string Albumart { get; set; }
+        public OpenTaikoChartInfomation_Courses[] Courses { get; set; }
+    }
+
+    public class OpenTaikoChartInfomation_Courses
+    {
+        public string Difficulty { get; set; }
+        public int? Level { get; set; }
+        public string Single { get; set; }
+        public string[] Multiple { get; set; }
+    }
+
+    public class OpenTaikoChartCourse
+    {
+        public int? Scoreinit { get; set; }
+        public int? Scorediff { get; set; }
+        public int? Scoreshinuchi { get; set; }
+        public int?[] Balloon { get; set; }
+        public string[][] Measures { get; set; }
+    }
+
+    public class OpenTaikoChart_Difficluty
+    {
+        public OpenTaikoChartCourse Single { get; set; }
+        public OpenTaikoChartCourse[] Multiple { get; set; }
+    }
+
+    public class OpenTaikoChart_Medley
+    {
+        public string Title { get; set; }
+        public OpenTaikoChart_Medley_Exams[] Exams { get; set; }
+
+        public OpenTaikoChart_Medley_Charts[] Charts { get; set; }
+    }
+
+    public class OpenTaikoChart_Medley_Exams
+    {
+        public string Type { get; set; }
+        public string Range { get; set; }
+        public int[] Value { get; set; }
+    }
+
+    public class OpenTaikoChart_Medley_Charts
+    {
+        public string File { get; set; }
+        public string Difficulty { get; set; }
+    }
+}
